05eae13d6a7545a651d646c8f3f106a1279eb39b
Chung Thế Hào <chungthehao@gmail.com>
2019-04-06 23:12:09 +0700

    Fetching Route Parameters Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,18 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'], // Ở app module "'users/:id'"
       name: this.route.snapshot.params['name']
     };
+
+    /**
+     * - Mặc định nếu dùng routerLink mà hiện tại đã ở component đó thì Angular ko tải lại component.
+     * - Nên các tham số trên URL có thay đổi, Angular cũng "ko biết"
+     * - Dùng observable để khi 'có gì đó xảy ra trong tương lai' thì nó sẽ chạy 'code mình quy định'. (async)
+     */
+    this.route.params.subscribe(
+      (newParams: Params) => {
+        this.user.id = newParams.id;
+        this.user.name = newParams.name;
+      }
+    );
   }
 
 }
