32c63c63163e6512ef45d2dfa8f0ab3e3d8a4651
Chung Thế Hào <chungthehao@gmail.com>
2019-04-07 08:51:52 +0700

    Retrieving Query Parameters and Fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,25 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /**
+     * # Cách 1:
+     * - Dùng snapshot chỉ 'bắt đc thông tin' khi component đc tạo ra (ngOnInit)
+     * - Khi đã ở component này rồi mà query string hoặc hash thay đổi sẽ ko 'bắt' đc thông tin
+     */
+    console.log(this.route.snapshot.queryParams)
+    console.log(this.route.snapshot.fragment)
+    /**
+     * # Cách 2:
+     * - Dùng observable
+     * - Ko cần unsubscribe ở life cycle hook 'destroy component', Angular do it for you!
+     */
+    this.route.queryParams.subscribe();
+    this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
