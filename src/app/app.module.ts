a8bc56fa41acc64c482a6b7a5cea6565b2464d43
Chung Thế Hào <chungthehao@gmail.com>
2019-04-07 18:02:38 +0700

    Redirecting and Wildcard Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,24 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },             // localhost:4200
+
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent },   // localhost:4200/users/1
   ] },       // localhost:4200/users
+
   { path: 'servers', component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent },
   ] },
+
+  { path: 'not-found', component: PageNotFoundComponent },
+
+  // Phải ở cuối cùng, khi Angular tìm ko thấy thằng nào match ở trên hết mới gặp dòng này! Vì ** match với mọi route
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -32,7 +40,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
