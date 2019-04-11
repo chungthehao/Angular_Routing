84ad1561dde0690c902865214e0d9510a48abecd
Chung Thế Hào <chungthehao@gmail.com>
2019-04-11 21:47:33 +0700

    Resolving Dynamic Data with the resolve Guard

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,17 +16,31 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    // Dấu '+' để ép nó về kiểu số (nó đang là kiểu string)
-    const serverId = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(serverId);
-
-    // Also want to react to any changes thereafter (dùng observable)
-    this.route.params.subscribe(
-      (newParams: Params) => {
-        // Dấu '+' để ép nó về kiểu số (nó đang là kiểu string)
-        this.server = this.serversService.getServer(+newParams['id']);
+    /**
+     * Cách 2:
+     * - Load data ngay khi route đc load (Angular tự chạy) rồi truyền thông qua resolve (xem thêm ở app-routing.module.ts)
+     * - It's an alternative - it allows you to load data BEFORE you render a component (server-resolver.service.ts)
+     */
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server']; // ở app-routing.module.ts resolve: {server: ServerResolver} lưu ý tên server phải match nhau
       }
     );
+
+    /**
+     * Cách 1: 
+     */
+    // // Dấu '+' để ép nó về kiểu số (nó đang là kiểu string)
+    // const serverId = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(serverId);
+
+    // // Also want to react to any changes thereafter (dùng observable)
+    // this.route.params.subscribe(
+    //   (newParams: Params) => {
+    //     // Dấu '+' để ép nó về kiểu số (nó đang là kiểu string)
+    //     this.server = this.serversService.getServer(+newParams['id']);
+    //   }
+    // );
   }
 
   onEdit() {
